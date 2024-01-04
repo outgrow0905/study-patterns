b98fa8d0815f65fb8f8d2d096737b4c475af2bdd
Juhoon Kim <nb11866@navercorp.com>
2024-01-05 07:31:32 +0900

    object chapter 1 theater example

---
+++ src/main/java/com/example/object/ch1/v1/Ticket.java
@@ -0,0 +1,9 @@
+package com.example.object.ch1.v1;
+
+public class Ticket {
+	private Long fee;
+
+	public Long getFee() {
+		return fee;
+	}
+}
---
+++ src/main/java/com/example/object/ch1/v1/TicketSeller.java
@@ -0,0 +1,13 @@
+package com.example.object.ch1.v1;
+
+public class TicketSeller {
+	private TicketOffice ticketOffice;
+
+	public TicketSeller(TicketOffice ticketOffice) {
+		this.ticketOffice = ticketOffice;
+	}
+
+	public TicketOffice getTicketOffice() {
+		return ticketOffice;
+	}
+}
